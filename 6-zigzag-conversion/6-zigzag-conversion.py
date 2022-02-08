76d1886fccb2ecd4dcb61a64f48c1ab1e5844498
Ashraf Hesham <62557094+ashrafhesham1@users.noreply.github.com>
2022-02-08 13:57:31 +0200

    Time: 80 ms (52.90%), Space: 14 MB (96.38%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.py
+++ 6-zigzag-conversion/6-zigzag-conversion.py
@@ -2,31 +2,11 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows==1:
             return s
-        arr_str=[None]*numRows
-        for i in range(len(arr_str)):
-            arr_str[i]=[None]*len(s)
-        dir=0
-        row,col=0,0
-        for i in range(len(s)):
-            if dir==0:
-                arr_str[row][col]=s[i]
-                if row<numRows-1:
-                    row+=1
-                else:
-                    row-=1
-                    col+=1
-                    dir=1
-            else:
-                arr_str[row][col]=s[i]
-                if row>0:
-                    row-=1
-                    col+=1
-                else:
-                    row+=1
-                    dir=0
         res=""
-        for i in range(len(arr_str)):
-            for j in range(len(arr_str[i])):
-                if arr_str[i][j]!=None:
-                    res+=arr_str[i][j]
+        inc=(numRows-1)*2 
+        for r in range(numRows):
+            for i in range(r,len(s),inc):
+                res+=s[i]
+                if r>0 and r<numRows-1 and i+inc-(2*r)<len(s):
+                    res+=s[i+inc-(2*r)] 
         return res
