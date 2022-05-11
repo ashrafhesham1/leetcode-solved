7adbd7a5d0eac6906dbeb45f2e8a9c2b04227ced
Ashraf Hesham <62557094+ashrafhesham1@users.noreply.github.com>
2022-05-11 12:45:42 +0200

    Time: 65 ms (56.73%), Space: 14 MB (36.00%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -0,0 +1,32 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        romans = {
+            1:'I',4:'IV',5:'V',9:'IX',10:'X',40:'XL',50:'L',90:'XC',100:'C',400:'CD',500:'D',900:'CM',1000:'M'
+        }
+        i=0
+        roman = ''
+        while num > 0 :
+
+            num, cur = divmod(num,10)
+            cur *= 10 ** i
+
+            cur_roman = ''
+            while cur > 0 :
+                if cur in romans :
+                    cur_roman =  cur_roman + romans[cur]
+                    break
+                else:
+                    keys = list(romans.keys())
+                    for j in range(len(keys)-1):
+                        if keys[j+1] > cur :
+                            cur_roman = cur_roman + romans[keys[j]]
+                            cur -= keys[j]
+                            break
+                        elif j+1 == len(keys)-1 :
+                            cur_roman = cur_roman + romans[keys[j+1]]
+                            cur -= keys[j+1]
+                            break
+
+            roman = cur_roman + roman
+            i+=1
+        return roman
