6ba752684e9a18c2147b22f7123826fb6432bd70
Ashraf Hesham <62557094+ashrafhesham1@users.noreply.github.com>
2022-05-11 13:11:49 +0200

    Time: 53 ms (80.09%), Space: 13.8 MB (81.13%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.py
+++ 12-integer-to-roman/12-integer-to-roman.py
@@ -1,6 +1,29 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        romans = {
+        romans = [[1,'I'],[4,'IV'],[5,'V'],[9,'IX'],[10,'X'],[40,'XL'],
+                  [50,'L'],[90,'XC'],[100,'C'],[400,'CD'],[500,'D'],
+                  [900,'CM'],[1000,'M']]
+        
+        roman = ''
+        for val, rom in reversed(romans):
+            if num // val :
+                count = num // val
+                roman += (rom*count)
+                num = num % val
+        return roman
+
+
+
+
+
+
+
+
+
+
+
+    '''   
+    romans = {
             1:'I',4:'IV',5:'V',9:'IX',10:'X',40:'XL',50:'L',90:'XC',100:'C',400:'CD',500:'D',900:'CM',1000:'M'
         }
         i=0
@@ -30,3 +53,4 @@ class Solution:
             roman = cur_roman + roman
             i+=1
         return roman
+        '''
